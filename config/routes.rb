67765ca45afdbfaa4fd77e1ed940f0f146f9f097
alinusgallus <gall.alain@gmail.com>
2014-04-04 23:03:48 +0800

    finish signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SampleApp::Application.routes.draw do
+  resources  :users
+
   root  'static_pages#home'
 
-  get "users/new"
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
