a6a653573ad80cc9707ff674d8444d31c111ca33
alinusgallus <gall.alain@gmail.com>
2014-04-27 15:56:11 +0800

    finish user micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
   end
 
   def show
-   @user = User.find(params[:id])
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def index
@@ -62,12 +63,7 @@ class UsersController < ApplicationController
     end
 
     #before filters
-    def signed_in_user
-      unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
-    end
-    end 
+    
 
     def correct_user
       @user = User.find(params[:id])
